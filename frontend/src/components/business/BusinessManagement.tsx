aed22af0b3c58348d04d16c4d8641de164e7bbb9
teamworkfission <teamworkfission@gmail.com>
2025-09-30 11:42:21 -0500

    Update rejected businesses section message to direct users to admin comments first

--- frontend/src/components/business/BusinessManagement.tsx
+++ frontend/src/components/business/BusinessManagement.tsx
@@ -377,7 +377,7 @@ export function BusinessManagement({ onBack }: BusinessManagementProps) {
                             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                           </svg>
                           <p className="text-sm text-red-800">
-                            <strong>These business applications have been rejected.</strong> Please contact support for more information about reapplying.
+                            <strong>These business applications have been rejected.</strong> See the admin comment below for details, or contact support for more information about reapplying.
                           </p>
                         </div>
                       </div>
